c8b909631c7ef5ff00651b432f48826079e2b963
Shamyle <shamyleg@gmail.com>
2016-11-15 16:27:15 -0600

    Implement one-to-many association

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Publisher, :type => :model do
   #Validations
   it {should validate_presence_of(:name)}
   it {should validate_uniqueness_of(:name).case_insensitive}
-
+  it {should have_many(:books)}
 
 end
