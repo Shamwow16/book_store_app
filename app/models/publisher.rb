4ba4678d7d896e84b8d87a0668dd36e4d25058cf
Shamyle <shamyleg@gmail.com>
2016-11-15 16:36:55 -0600

    Add publication model

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,6 +1,10 @@
 class Publisher < ActiveRecord::Base
-
+  has_many :books
   validates :name, presence: true
   validates :name, uniqueness: {case_sensitive: false}
-  has_many :books
+
+  belongs_to :author
+  belongs_to :book
+
+  has_many :publications
 end
