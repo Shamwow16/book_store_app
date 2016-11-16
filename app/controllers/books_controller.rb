c06104a18c2e81ac9590e3f8aa2695b0b2034718
Shamyle <shamyleg@gmail.com>
2016-11-16 13:26:04 -0600

    Refactor before_action in book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,12 @@
 class BooksController < ApplicationController
+
+  before_action :set_book, except: [:index,:new,:create]
+
   def index
+    @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
@@ -23,11 +26,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update_attributes(book_params)
       flash[:success] = "Book has been updated"
       redirect_to @book
@@ -38,7 +39,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     if @book.destroy
       flash[:success] = "Book has been deleted"
       redirect_to books_path
@@ -48,6 +48,10 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :price, :isbn, :page_count, :description, :published_at, :publisher_id)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 
 end
