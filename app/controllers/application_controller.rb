0502af696b00a0080d010803add91df4889d174f
Shamyle <shamyleg@gmail.com>
2016-12-19 14:24:41 -0600

    Restrict access to guest users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
+
+  def require_signin
+    unless current_user
+      flash[:danger] = 'Please sign in first!'
+      redirect_to signin_path
+    end
+  end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,119 +1,137 @@
-require "rails_helper"
+require 'rails_helper'
+require 'support/macros'
 
-RSpec.describe AuthorsController, :type => :controller do
-  describe "GET #index" do
-    it "returns a successful http request status code" do
-      get :index
-      expect(response).to have_http_status(:success)
+RSpec.describe AuthorsController, type: :controller do
+  let(:admin) { Fabricate(:admin) }
+  let(:user) { Fabricate(:user) }
+
+  before { set_current_admin admin }
+
+  describe 'GET #index' do
+    context 'guest users' do
+      before { clear_current_user }
+
+      it 'redirects to the signin page for un-authenticated users' do
+        get :index
+        expect(response).to redirect_to signin_path
+      end
+    end
+
+    context 'non-admin users' do
+    end
+
+    context 'admin users' do
+      it 'returns a successful http request status code' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  describe "GET #show" do
-    it "returns a successful http request status code"  do
+  describe 'GET #show' do
+    it 'returns a successful http request status code' do
       author = Fabricate.create(:author)
       get :show, id: author.id
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #new" do
-    it "returns a successful http request status code" do
+  describe 'GET #new' do
+    it 'returns a successful http request status code' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "POST #create" do
-    context "successful create" do
-      it "saves the new author object" do
+  describe 'POST #create' do
+    context 'successful create' do
+      it 'saves the new author object' do
         post :create, author: Fabricate.attributes_for(:author)
 
         expect(Author.count).to eq(1)
       end
 
-      it "redirects to the author show action" do
+      it 'redirects to the author show action' do
         post :create, author: Fabricate.attributes_for(:author)
         expect(response).to redirect_to author_path(Author.first)
-
       end
 
-      it "sets the success flash message" do
+      it 'sets the success flash message' do
         post :create, author: Fabricate.attributes_for(:author)
-        expect(flash[:success]).to eq("Author has been created")
-
+        expect(flash[:success]).to eq('Author has been created')
       end
     end
 
-    context "unsuccessful create" do
-      it "does not save the author object with invalid input" do
+    context 'unsuccessful create' do
+      it 'does not save the author object with invalid input' do
         post :create, author: Fabricate.attributes_for(:author, first_name: nil)
         expect(Author.count).to eq(0)
       end
 
-      it "sets the failure flash message" do
+      it 'sets the failure flash message' do
         post :create, author: Fabricate.attributes_for(:author, first_name: nil)
-        expect(flash.now[:danger]).to eq("Author has not been created")
+        expect(flash.now[:danger]).to eq('Author has not been created')
       end
     end
   end
 
-  describe "GET #edit" do
-    let(:author) {Fabricate(:author)}
+  describe 'GET #edit' do
+    let(:author) { Fabricate(:author) }
 
-    it "sends a successful edit request" do
+    it 'sends a successful edit request' do
       get :edit, id: author
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "PUT #update" do
-    context "successful update" do
-      let(:john) {Fabricate(:author, first_name: "John")}
-      it "updates the modified author object" do
-        put :update, author: Fabricate.attributes_for(:author, first_name:"Mike"), id: john.id
-        expect(Author.last.first_name).to eq("Mike")
-        expect(Author.last.first_name).not_to eq("John")
+  describe 'PUT #update' do
+    context 'successful update' do
+      let(:john) { Fabricate(:author, first_name: 'John') }
+      it 'updates the modified author object' do
+        put :update, author: Fabricate.attributes_for(:author, first_name: 'Mike'), id: john.id
+        expect(Author.last.first_name).to eq('Mike')
+        expect(Author.last.first_name).not_to eq('John')
       end
 
-      it "sets the success flash message" do
-        put :update, author: Fabricate.attributes_for(:author, first_name:"Mike"), id: john.id
-        expect(flash[:success]).to eq("Author has been updated")
+      it 'sets the success flash message' do
+        put :update, author: Fabricate.attributes_for(:author, first_name: 'Mike'), id: john.id
+        expect(flash[:success]).to eq('Author has been updated')
       end
-      it "redirects to the show action" do
-        put :update, author: Fabricate.attributes_for(:author, first_name:"Mike"), id: john.id
+      it 'redirects to the show action' do
+        put :update, author: Fabricate.attributes_for(:author, first_name: 'Mike'), id: john.id
         expect(response).to redirect_to author_path(Author.last)
       end
     end
 
-    context "unsuccessful update" do
-      let(:john) {Fabricate(:author, first_name: "John")}
-      it "does not update the author object with invalid inputs" do
+    context 'unsuccessful update' do
+      let(:john) { Fabricate(:author, first_name: 'John') }
+      it 'does not update the author object with invalid inputs' do
         put :update, author: Fabricate.attributes_for(:author, first_name: nil), id: john.id
-        expect(Author.last.first_name).to eq("John")
+        expect(Author.last.first_name).to eq('John')
       end
 
-      it "sets the failure flash message" do
+      it 'sets the failure flash message' do
         put :update, author: Fabricate.attributes_for(:author, first_name: nil), id: john.id
-        expect(flash.now[:danger]).to eq("Author has not been updated")
+        expect(flash.now[:danger]).to eq('Author has not been updated')
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    let(:author) {Fabricate(:author)}
+  describe 'DELETE #destroy' do
+    let(:author) { Fabricate(:author) }
 
-    it "deletes the author with given id" do
+    it 'deletes the author with given id' do
       delete :destroy, id: author.id
       expect(Author.count).to eq(0)
     end
 
-    it "sets the flash message" do
+    it 'sets the flash message' do
       delete :destroy, id: author.id
-      expect(flash[:success]).to eq("Author has been deleted")
+      expect(flash[:success]).to eq('Author has been deleted')
     end
 
-    it "redirects to the index action" do
-      delete :destroy, id:author.id
+    it 'redirects to the index action' do
+      delete :destroy, id: author.id
       expect(response).to redirect_to authors_path
     end
   end
